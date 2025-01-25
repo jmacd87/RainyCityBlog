49b46308af0821367825688c9b468b54d25958ec
Joel MacDonald <jmacd87@gmail.com>
2025-01-25 12:21:22 -0800

    fix local route

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/posts',
+  baseURL: `${process.env.VITE_BACKEND_BASE_URL}/api/posts`,
 });
 
 export default api;
