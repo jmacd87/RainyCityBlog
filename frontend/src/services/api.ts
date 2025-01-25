dd52a944b90d2b3bba57e5e97d5992beb03140a9
Joel MacDonald <jmacd87@gmail.com>
2025-01-25 12:25:47 -0800

    fix env imports

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${process.env.VITE_BACKEND_BASE_URL}/api/posts`,
+  baseURL: `${import.meta.env.VITE_BACKEND_BASE_URL}/api/posts`,
 });
 
 export default api;
