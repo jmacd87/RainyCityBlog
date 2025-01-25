e88f3a4639108ecc104dc7b4a643211f3c8292df
Joel MacDonald <jmacd87@gmail.com>
2025-01-25 13:06:09 -0800

    use frontend URL

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,11 @@ const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
 connectDB();
 
 // Middleware setup
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // Allow specific origin
+  })
+);
 app.use(express.json());
 app.use('/api/posts', postsRouter);
 
