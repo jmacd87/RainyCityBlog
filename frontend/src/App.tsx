7472b94646832444add911331211a178a35c6e57
Joel MacDonald <jmacd87@gmail.com>
2025-02-10 10:15:45 -0800

    add vercel analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import AppRouter from './routes/Router';
 import './App.css';
 import { PostsProvider } from './contexts/PostsContext';
+import { Analytics } from '@vercel/analytics/next';
 
 const App: React.FC = () => (
   <PostsProvider>
     <AppRouter />
+    <Analytics />
   </PostsProvider>
 );
 
--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from '../components/Layout';
-import HomePage from '../pages/HomePage';
-import StoryPage from '../pages/StoryPage';
 import ContactPage from '../pages/ContactPage';
 import ScrollToTop from '../helpers/ScrollToTop';
+import LoadingScreen from '../components/LoadingScreen';
 import '../App.css';
 
-const AppRouter: React.FC = () => (
-  <Router>
-    <ScrollToTop />
-    <Layout>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/story/:id" element={<StoryPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
-    </Layout>
-  </Router>
-);
+const AppRouter: React.FC = () => {
+  const HomePage = React.lazy(() => import('../pages/HomePage'));
+  const StoryPage = React.lazy(() => import('../pages/StoryPage'));
+  return (
+    <Router>
+      <ScrollToTop />
+      <Layout>
+        <Suspense fallback={<LoadingScreen />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/story/:id" element={<StoryPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </Suspense>
+      </Layout>
+    </Router>
+  );
+};
 
 export default AppRouter;
